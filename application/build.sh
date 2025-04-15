0e80f26b187e7a1ca2b3be9b93b29f042ae03bd4
jrakotomalala <jrakotomalala.innovqube.com>
2025-04-15 19:32:00 +0200

    test n°5

--- application/build.sh
+++ application/build.sh
@@ -3,13 +3,13 @@ touch build.sh
 chmod +x build.sh
 echo -e '#!/bin/bash\necho "Build réussi 🚀"' > build.sh
 
-git add build.sh
+#git add build.sh
 #!/bin/bash
 
 # Config Git (à adapter avec ton nom/pseudo GitHub)
 git config --global user.name "Jocerak"
 git config --global user.email "jocerak@yahoo.fr"
 
-git commit -m "Ajout du script build.sh"
-git push origin main
+#git commit -m "Ajout du script build.sh"
+#git push origin main
 
